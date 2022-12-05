b25a76b8106f63b518aa42fe3d0dbe6248d9d720
AndreMaia95 <andrefsm95@gmail.com>
2022-12-05 17:45:23 +0000

    feat: add language option

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -12,9 +11,9 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    TranslateModule,
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
+import { Preferences } from '@capacitor/preferences';
+import { TranslateService } from '@ngx-translate/core';
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(
+    private translateService: TranslateService,
+    private toastController: ToastController
+  ) {}
 
-  constructor() {}
+  async changeLanguage(language: string) {
+    await Preferences.set({ key: 'user-lang', value: language });
+    await this.showToast();
+  }
 
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: this.translateService.instant('languagessuccessfullychanges'),
+      duration: 4000,
+    });
+    await toast.present();
+  }
 }
