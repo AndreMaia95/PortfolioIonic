8503e7dbfc1db9a4a235a08bcdc87d7a7a42c54f
AndreMaia95 <andrefsm95@gmail.com>
2022-12-15 11:27:42 +0000

    fix: tab1 information for the API

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { LocalizationService } from './../services/localization/localization.service';
 import { Preferences } from '@capacitor/preferences';
 import { TranslateService } from '@ngx-translate/core';
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -9,19 +12,30 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  me: any = {};
+
   constructor(
     private translateService: TranslateService,
-    private toastController: ToastController
+    private toastController: ToastController,
+    private LocalizationService: LocalizationService,
+    private http: HttpClient
   ) {}
+
+  async ngOnInit() {
+    this.me = await firstValueFrom(
+      this.http.get('http://localhost:4242/api/me')
+    );
+  }
 
   async changeLanguage(language: string) {
     await Preferences.set({ key: 'user-lang', value: language });
+    await this.LocalizationService.setLanguage(language);
     await this.showToast();
   }
 
   async showToast() {
     const toast = await this.toastController.create({
-      message: this.translateService.instant('languagessuccessfullychanges'),
+      message: this.translateService.instant('language as been changed'),
       duration: 4000,
     });
     await toast.present();
