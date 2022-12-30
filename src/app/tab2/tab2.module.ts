500243a8b1289bbfbd85ad7980172944ac7aecac
AndreMaia95 <andrefsm95@gmail.com>
2022-12-30 18:33:39 +0000

    feat: about me page and connect api

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,17 +4,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    TranslateModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page],
 })
 export class Tab2PageModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { firstValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { LocalizationService } from './../services/localization/localization.service';
+import { Preferences } from '@capacitor/preferences';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-tab2',
@@ -6,6 +12,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  me: any = {};
+
+  constructor(
+    private translateService: TranslateService,
+    private toastController: ToastController,
+    private LocalizationService: LocalizationService,
+    private http: HttpClient
+  ) {}
+
+  async ngOnInit() {
+    this.me = await firstValueFrom(
+      this.http.get('http://localhost:4242/api/me')
+    );
+  }
+
+  async changeLanguage(language: string) {
+    await Preferences.set({ key: 'user-lang', value: language });
+    await this.LocalizationService.setLanguage(language);
+    await this.showToast();
+  }
+
+  async showToast() {
+    const toast = await this.toastController.create({
+      message: this.translateService.instant('language as been changed'),
+      duration: 4000,
+    });
+    await toast.present();
+  }
+
   isModalOpen = false;
 
   setOpen(isOpen: boolean) {
